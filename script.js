625ef8ff55ffe926984a73fa1cfb0025f35a3b0b
Ingwai <ingwai@wp.pl>
2023-09-19 23:03:04 +0200

    add section nasze biura with google maps and optimized jpgs

--- script.js
+++ script.js
@@ -15,15 +15,48 @@ cards.forEach((card, index) => {
 	});
 });
 
-const header = document.querySelector('[data-header]');
-const goTopBtn = document.querySelector('[data-go-top]');
+const header = document.querySelector('.header');
+const goToTopBtn = document.querySelector('.go-top');
 
 window.addEventListener('scroll', function () {
 	if (window.scrollY >= 10) {
 		header.classList.add('active');
-		goTopBtn.classList.add('active');
+		goToTopBtn.classList.add('active');
 	} else {
 		header.classList.remove('active');
-		goTopBtn.classList.remove('active');
+		goToTopBtn.classList.remove('active');
 	}
 });
+
+/**
+ * Tab content
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+	for (let i = 0, len = elements.length; i < len; i++) {
+		elements[i].addEventListener(eventType, callback);
+	}
+};
+
+const tabBtns = document.querySelectorAll('[data-tab-btn]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+let lastActiveTabBtn = tabBtns[0];
+let lastActiveTabContent = tabContents[0];
+
+const filterContent = function () {
+	if (!(lastActiveTabBtn === this)) {
+		lastActiveTabBtn.classList.remove('active');
+		lastActiveTabContent.classList.remove('active');
+
+		this.classList.add('active');
+		lastActiveTabBtn = this;
+
+		const currentTabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
+
+		currentTabContent.classList.add('active');
+		lastActiveTabContent = currentTabContent;
+	}
+};
+
+addEventOnElements(tabBtns, 'click', filterContent);
